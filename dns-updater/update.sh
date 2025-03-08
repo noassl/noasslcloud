484a71561f3b7e54f33969f3ee0d0dbe08cc8f34
Janik <jneissl27@gmail.com>
2025-03-08 16:10:04 +0100

    Update DNS update script to support multiple records

--- dns-updater/update.sh
+++ dns-updater/update.sh
@@ -2,7 +2,7 @@
 
 # env file sourcen: https://stackoverflow.com/a/43267603/11864516
 set -a
-source .env
+source .env.sh
 set +a
 
 current_ip=$(curl -s 'https://api.ipify.org')
@@ -14,16 +14,20 @@ fi
 
 export current_cloudflare_ip="$current_ip"
 
-curl --request PUT \
-  --url https://api.cloudflare.com/client/v4/zones/ae6319a2837df20a106a61e11313816c/dns_records/6ce39725cda6ec002f7e0d61add587e1 \
-  --header "Content-Type: application/json" \
-  --header "X-Auth-Email: $CLOUDFLARE_EMAIL" \
-  --header "Authorization: Bearer $CLOUDFLARE_TOKEN" \
-  --data '{
-  "comment": "Automatic update (NUC)",
-  "name": "noassl.at",
-  "proxied": true,
-  "ttl": 1,
-  "content": "'"$current_ip"'",
-  "type": "A"
-}'
+for record in "${!DNS_RECORDS[@]}"; do
+  name=${DNS_RECORDS[$record]}
+
+  curl --request PUT \
+    --url "https://api.cloudflare.com/client/v4/zones/ae6319a2837df20a106a61e11313816c/dns_records/${record}" \
+    --header "Content-Type: application/json" \
+    --header "X-Auth-Email: $CLOUDFLARE_EMAIL" \
+    --header "Authorization: Bearer $CLOUDFLARE_TOKEN" \
+    --data '{
+      "comment": "Automatic update (NUC)",
+      "name": "'"$name"'",
+      "proxied": true,
+      "ttl": 1,
+      "content": "'"$current_ip"'",
+      "type": "A"
+    }'
+done
