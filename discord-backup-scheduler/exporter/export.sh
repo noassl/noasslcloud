1afe16b78f75f2dbd97343493de2e24c0151bda8
Janik <jneissl27@gmail.com>
2025-08-17 23:08:23 +0200

    [dc-analytics] implement partial exports

--- discord-backup-scheduler/exporter/export.sh
+++ discord-backup-scheduler/exporter/export.sh
@@ -1,19 +1,15 @@
 #!/bin/bash
 
 workDir="/app"
-
 mkdir -p "$workDir"
 
 function exportPartial {
-  files=(/out/*.json)
-  firstFile="${files[0]}"
+  lastExportFile=$(ls -1t /exports/*.json | head -n 1)
 
-  echo "Reading export from $firstFile."
-
-  lastExport=$(jq -r .exportedAt "$firstFile")
+  echo "Reading export from $lastExportFile."
+  lastExport=$(jq -r .exportedAt "$lastExportFile")
 
   echo "Exporting messages since $lastExport"
-
   /app/exporter/DiscordChatExporter.Cli exportguild \
     -g "$2" \
     --format Json \
@@ -33,39 +29,17 @@ function exportFull {
     --include-vc true
 }
 
-#if [ ! -d $workDir ]; then
-#  echo "No input dir found at $workDir."
-#  exit 1
-#fi
+function main {
+  # Count json files in /out
+  numFiles=$(ls -1 /export/*.json | wc -l)
 
-#outFolder="/out/export-$(date +'%d.%m.%y-%H:%M:%S')"
-#mkdir "${outFolder}"
-
-# Count json files in /out
-#numFiles=$(ls -l /out/*.json | wc -l)
-
-# If no json files are in /out, there hasn't been a prior export, so a full one needs to be performed
-#if [ "$numFiles" -eq 0 ]; then
-#  exportFull "$workDir" "$GUILD_ID"
-#else
-#  exportPartial "$workDir" "$GUILD_ID"
-#fi
-
-# Über files in $outFolder loopn
-#for filepath in "${outFolder}"/*; do
-#  filename=$(basename "${filepath}")
-#  baseFile="/out/${filename}"
-#
-#  # Fia jedes file schaun obs in /out existiert
-#  if [[ -e $baseFile ]]; then
-#    # jo -> mergen
-#    # TODO: JSON merging: overwrite exportedAt, merge messages, sum messageCount
-#    true;
-#  else
-#    # na -> einfoch ausse kopieren
-#    cp "${filepath}" "${baseFile}"
-#  fi;
-#done
+  # If no json files are in /out, there hasn't been a prior export, so a full one needs to be performed
+  if [ "$numFiles" -eq 0 ]; then
+    exportFull "/exports" "$GUILD_ID"
+  else
+    exportPartial "/exports" "$GUILD_ID"
+  fi
+}
 
 # Nachrichten exportieren
-exportFull "/exports" "$GUILD_ID"
+main
