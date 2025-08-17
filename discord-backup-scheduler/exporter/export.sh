a2515aeab8ec76e2cdd1ee92ebaa66ddcddbe67e
Janik <jneissl27@gmail.com>
2025-08-17 23:15:52 +0200

    [dc-analytics] bugfix partial export

--- discord-backup-scheduler/exporter/export.sh
+++ discord-backup-scheduler/exporter/export.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
+exportsDir="/exports"
+
 workDir="/app"
 mkdir -p "$workDir"
 
 function exportPartial {
-  lastExportFile=$(ls -1t /exports/*.json | head -n 1)
+  lastExportFile=$(ls -1t "$exportsDir"/*.json | head -n 1)
 
   echo "Reading export from $lastExportFile."
   lastExport=$(jq -r .exportedAt "$lastExportFile")
@@ -31,13 +33,13 @@ function exportFull {
 
 function main {
   # Count json files in /out
-  numFiles=$(ls -1 /export/*.json | wc -l)
+  numFiles=$(ls -1 "$exportsDir"/*.json | wc -l)
 
   # If no json files are in /out, there hasn't been a prior export, so a full one needs to be performed
   if [ "$numFiles" -eq 0 ]; then
-    exportFull "/exports" "$GUILD_ID"
+    exportFull "$exportsDir" "$GUILD_ID"
   else
-    exportPartial "/exports" "$GUILD_ID"
+    exportPartial "$exportsDir" "$GUILD_ID"
   fi
 }
 
